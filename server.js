e1e3fb757301047636103fc4f56eebc03c78f57d
Ali Safdar Kureishy <safdar.kureishy@gmail.com>
2018-11-23 23:25:32 -0800

    Fixed server port

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const BlockChainServer = require('./blockChainServer.js').BlockChainServer;
  * This is the entry point for launching the server on the command-line
  * ============================================================
  */
-let server = new BlockChainServer("chaindata", 1234);
+let server = new BlockChainServer("chaindata", 8000);
 server.start();
